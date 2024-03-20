51d4aa92bf1db3c3a6acb576c9b558d76a86d257
aria-1337 <aria.lopez.dev@gmail.com>
2024-03-20 00:09:48 -0700

    Remember keypress

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,38 @@ import styled from 'styled-components';
 import Line from './components/Line';
 
 export default function App() {
-    const [editor, setEditor] = useState({
-        selectedRow: 1,
-        cursorPos: 0,
-        rows: [{ n: 1, text: 'some default text' }]
-    });
+    // Editor Logic
+    const [rows, setRows] = useState([{ n: 1, text: 'some default text' }]);
+    const [keypresses, setKeypresses] = useState<Array<number>>([]);
+    const [selectedRow, setSelectedRow] = useState<number>(0);
+    const [cursorPos, setCursorPos] = useState<number>(0);
+
+    useEffect(() => {
+        // MOUNT
+        document.addEventListener('keydown', (e) => {
+            setKeypresses([...keypresses, e.keyCode]);
+        });
+        // UNMOUNT
+        return () => {
+            document.removeEventListener('keydown', () => {});
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log(keypresses);
+    }, [keypresses]);
+
+    // @addLine()
+    // This function created a new line is initially appended to the last Line instance.
+    // It should be called during the following scenarios:
+    // [] Enter press on keyboard -> Enters line @ n+1
+    function addLine() {
+    }
+
     return (<>
     <h1>Vim</h1>
     <EditorWrapper>
-        { editor?.rows?.map((r, key) => <Line key={key} n={r.n} text={r.text} />) }
+        { rows?.map((r, key) => <Line key={key} n={r.n} text={r.text} />) }
         <Line key={'end'} n={0} text={''} />
     </EditorWrapper>
     </>);
