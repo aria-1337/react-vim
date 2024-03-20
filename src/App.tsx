7755526c0b0b12a9ec443fa7b068fac95d34b2e2
aria-1337 <aria.lopez.dev@gmail.com>
2024-03-19 23:02:50 -0700

    Added basic line style and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Line from './components/Line';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [editor, setEditor] = useState({
+        selectedRow: 1,
+        cursorPos: 0,
+        rows: [{ n: 1, text: 'some default text' }]
+    });
+    return (<>
+    <h1>Vim</h1>
+    <EditorWrapper>
+        { editor?.rows?.map((r, key) => <Line key={key} n={r.n} text={r.text} />) }
+        <Line key={'end'} n={0} text={''} />
+    </EditorWrapper>
+    </>);
 }
 
-export default App;
+const EditorWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    padding: 5px;
+`;
---
+++ src/components/Line.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+interface LineProps {
+    n: number;
+    text: string; 
+}
+
+export default function Line({ n, text } : LineProps) {
+    const [v, setV] = useState(text);
+    function handleTyping(e: React.ChangeEvent<HTMLInputElement>) {
+        setV(e?.target?.value);
+    }
+
+    return (<LineContainer>
+        <LineNumber>{ n === 0 ? '~' : n }</LineNumber>
+        <LineText value={v} onChange={(e) => handleTyping(e)}/>
+    </LineContainer>);
+}
+
+const LineContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const LineNumber = styled.p`
+    margin: 1px;
+    color: red;
+    font-weight: bold;
+`;
+
+const LineText = styled.input`
+    margin: 1px;
+    border: none;
+    font-family: inherit;
+    font-weight: inherit;
+    font-size: 14px;
+`;
